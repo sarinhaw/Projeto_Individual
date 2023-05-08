12aa7536953445c72607125a0bf446820a1a6001
sarinhaw <sarah.oliveira@sptech.school>
2023-05-08 13:10:17 -0300

    Adicionando a home e simulador

--- Vincent's Vision/site/src/database/config.js
+++ Vincent's Vision/site/src/database/config.js
@@ -19,10 +19,10 @@ var sqlServerConfig = {
 
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
-    host: "SEU_SERVIDOR",
-    database: "SEU_BANCO_DE_DADOS",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
+    host: "localhost",
+    database: "VS",
+    user: "root",
+    password: "1234.s",
 };
 
 function executar(instrucao) {
